a00be1b469a29a0463b1b8bafb7830c59142787e
Rhea-Gomez <rhea.gomez@aurionpro.com>
2025-07-07 15:09:48 +0530

    Assignment on Pig Dice Game

---
+++ PigDiceGame/PigDiceGame/PigDice.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PigDiceGame
+{
+    internal class PigDice
+    {
+        public static int turnScore = 0;
+        public static int turn = 1;
+        public static int totalScore = 0;
+        
+
+        public static void RollADie()
+        {
+            do
+            {
+                turnScore = 0;
+                Console.WriteLine();
+                Console.WriteLine("Turn: " + turn);
+                
+               
+                do
+                {
+                    Console.WriteLine("Would you like to roll a die or hold a die? (r/h)");
+                    string action = Console.ReadLine();
+                    Random random = new Random();
+                    int die = random.Next(1, 10);
+                    if (action == "r")
+                    { 
+                        Console.WriteLine("Die: " + die);
+                        if (die == 1)
+                        {
+                            turnScore = 0;
+                            Console.WriteLine("Turn over");
+                            Console.WriteLine("Turn {0} score: {1}", turn, turnScore);
+                            Console.WriteLine("Total Score: " + totalScore);
+                            turn++;
+                            Console.WriteLine();
+                            break;
+                        }
+                        else
+                        {
+                            turnScore += die;
+      
+                        }
+                    }
+                    else if (action == "h")
+                    {
+                        totalScore += turnScore;
+                        Console.WriteLine("Your Score for Turn {0}: {1}", turn, turnScore);
+                        Console.WriteLine("Total Score: " + totalScore);
+                        turn++;
+                        Console.WriteLine();
+                        break;
+                    }
+
+
+                } while (totalScore < 20);
+            } while(totalScore < 20);
+
+
+            
+           
+                Console.WriteLine($"You finished the game in {turn - 1} turns!");
+                Console.WriteLine("Total Score: " + totalScore);
+           
+        }
+    }
+}
---
+++ PigDiceGame/PigDiceGame/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace PigDiceGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the PigDice Game!");
+            Console.WriteLine("You have 3 turns to win the game");
+            Console.WriteLine("If your Total Score exceeds 20 within 3 turns, you win the game!");
+            Console.WriteLine("Press (r) to roll a die and Press (h) to hold a die.");
+            Console.WriteLine("Remember if you do not hold a die, you will lose all the points gained in that turn.");
+            Console.WriteLine("If the Die shows 1, your turn is over and you will lose all points gained in that turn.");
+            
+            PigDice pigDice = new PigDice();
+            PigDice.RollADie();
+        }
+    }
+}
